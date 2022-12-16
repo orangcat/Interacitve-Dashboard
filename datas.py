20f28810647f448813010dca8c7d26d8c2605970
orangcat <wangshouzhena@163.com>
2022-12-16 16:07:43 +0800

    commit

---
+++ datas.py
@@ -0,0 +1,76 @@
+covid_19_data = [
+    {'name': 'Senegal', 'value': [8.4, 1356436, 'Senegal']},
+    {'name': 'Croatia', 'value': [55.2, 2250353, 'Croatia']},
+    {'name': 'Switzerland', 'value': [70, 6011892, 'Switzerland']},
+    {'name': 'Portugal', 'value': [86.4, 8883561, 'Portugal']},
+    {'name': 'Netherlands', 'value': [68.5, 11957652, 'Netherlands']},
+    {'name': 'Australia', 'value': [84.4, 21656364, 'Australia']},
+    {'name': 'Poland', 'value': [58.9, 22607497, 'Poland']},
+    {'name': 'Morocco', 'value': [65.6, 23514674, 'Morocco']},
+    {'name': 'Argentina', 'value': [84.3, 37888014, 'Argentina']},
+    {'name': 'Spain', 'value': [86.3, 40657640, 'Spain']},
+    {'name': 'England', 'value': [76.6, 42864504, 'England']},
+    {'name': 'Korea', 'value': [86.3, 44705985, 'Korea']},
+    {'name': 'France', 'value': [79.2, 53120889, 'France']},
+    {'name': 'Japan', 'value': [81.8, 103050985, 'Japan']},
+    {'name': 'Brazil', 'value': [82.4, 174215643, 'Brazil']},
+    {'name': 'United States', 'value': [69.4, 228604758, 'United States']}
+]
+
+covid_19_data_ = [
+    {'name': 'Brazil', 'value': [82.4, 174215643, 'Brazil']},
+    {'name': 'Senegal', 'value': [8.4, 1356436, 'Senegal']},
+    {'name': 'Croatia', 'value': [55.2, 2250353, 'Croatia']},
+    {'name': 'Switzerland', 'value': [70, 6011892, 'Switzerland']},
+    {'name': 'Poland', 'value': [58.9, 22607497, 'Poland']},
+    {'name': 'Portugal', 'value': [86.4, 8883561, 'Portugal']},
+    {'name': 'United States', 'value': [69.4, 228604758, 'United States']},
+    {'name': 'Netherlands', 'value': [68.5, 11957652, 'Netherlands']},
+    {'name': 'Morocco', 'value': [65.6, 23514674, 'Morocco']},
+    {'name': 'Argentina', 'value': [84.3, 37888014, 'Argentina']},
+    {'name': 'France', 'value': [79.2, 53120889, 'France']},
+    {'name': 'Spain', 'value': [86.3, 40657640, 'Spain']},
+    {'name': 'Australia', 'value': [84.4, 21656364, 'Australia']},
+    {'name': 'England', 'value': [76.6, 42864504, 'England']},
+    {'name': 'Korea', 'value': [86.3, 44705985, 'Korea']},
+    {'name': 'Japan', 'value': [81.8, 103050985, 'Japan']},
+]
+
+country_name_map = {
+    'Ukraine': '乌克兰',
+    'Iceland': '冰岛',
+    'Gibraltar': '直布罗陀',
+    'Japan': '日本',
+    'France': '法国',
+    'Chile': '智利',
+    'Germany': '德国',
+    'Israel': '以色列',
+    'Russia': '俄罗斯',
+    'Brazil': '巴西',
+    'United Kingdom': '英国',
+    'Turkey': '土耳其',
+    'India': '印度',
+    'United States': '美国',
+    'China': '中国',
+
+
+    'Senegal': '塞内加尔',
+    'Croatia': '克罗地亚',
+    'Switzerland': '瑞士',
+    'Portugal': '葡萄牙',
+    'Netherlands': '荷兰',
+    'Australia': '澳大利亚',
+    'Poland': '波兰',
+    'Morocco': '摩洛哥',
+    'Argentina': '阿根廷',
+    'Spain': '西班牙',
+    'England': '英格兰',
+    'Korea': '韩国',
+    'France': '法国',
+    'Japan': '日本',
+    'Brazil': '巴西',
+    'United States': '美国'
+}
+
+# import random
+# random.shuffle(covid_19_data)  # 打乱顺序
